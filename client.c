e178bda0b81b394d4299f04b85b7690bf139ee97
mirsella <mirsella@protonmail.com>
2022-12-09 18:06:10 +0100

    WIP

--- client.c
+++ client.c
@@ -1,56 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 15:41:53 by mirsella          #+#    #+#             */
+/*   Updated: 2022/12/09 17:47:54 by mirsella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include "libft/libft.h"
 #include "unistd.h"
+#include "stdlib.h"
 
+int	g_counter = 0;
 
-void    sig_handler(int signo, siginfo_t *info, void *context)
+void	sig_handler(int signo)
 {
+	if (signo == SIGUSR1)
+	{
+		g_counter++;
+	}
+	else if (signo == SIGUSR2)
+	{
+		ft_printf("Encountered a error");
+		exit(1);
+	}
 }
 
-// https://openclassrooms.com/fr/courses/4117396-developpez-en-c-pour-lembarque/4633446-manipulez-les-registres-et-les-masques
+void	send_char(char c, int pid)
+{
+	int		bit;
+
+	// ft_printf("Sending char: %c\n", c);
+	bit = 128;
+	while (bit > 0)
+	{
+		if (c & bit)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		pause();
+		bit >>= 1;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	int		i;
-	int		bit;
-	int		pid;
-	char	c;
+	int					i;
 
 	if (argc != 3)
 	{
 		ft_printf("usage: ./client [pid] [string]\n");
-		return (0);
+		return (1);
 	}
-
-	struct sigaction act;
-
-	ft_printf("pid: %d\n", getpid());
-	sigemptyset(&act.sa_mask);
-	sigaddset(&act.sa_mask, SIGUSR1);
-	sigaddset(&act.sa_mask, SIGUSR2);
-	act.sa_sigaction = sig_handler;
-	act.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &act, NULL);
-	sigaction(SIGUSR2, &act, NULL);
-	while (1)
-	{
-		pause();
-	}
-	pid = ft_atoi(argv[1]);
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
 	i = 0;
 	while (argv[2][i])
-	{
-		c = argv[2][i];
-		bit = 128;
-		// ft_printf("c: %c, decimal: %d\n", c, c);
-		while (bit > 0)
-		{
-			if (c & bit)
-				kill(pid, SIGUSR2);
-			else
-				kill(pid, SIGUSR1);
-			bit >>= 1;
-			usleep(100);
-		}
-		i++;
-	}
+		send_char(argv[2][i++], ft_atoi(argv[1]));
+	send_char(argv[2][i++], ft_atoi(argv[1]));
+	if (g_counter == i * 8)
+		ft_printf("Message sent\n");
+	else
+		ft_printf("Message not sent. sent: %d, received %d\n", i, g_counter);
 }
--- server.c
+++ server.c
@@ -1,38 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 15:41:50 by mirsella          #+#    #+#             */
+/*   Updated: 2022/12/09 18:03:41 by mirsella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include "libft/libft.h"
 #include "unistd.h"
+#include "stdlib.h"
 
-// https://openclassrooms.com/fr/courses/4117396-developpez-en-c-pour-lembarque/4633446-manipulez-les-registres-et-les-masques
-void    sig_handler(int signo, siginfo_t *info, void *context)
+char	*g_str = NULL;
+
+char	*store_char(char c, int reset)
+{
+	static size_t			mallocsize = 25;
+	char					*tmp;
+
+	if (reset)
+	{
+		mallocsize = 25;
+		free(g_str);
+		g_str = NULL;
+	}
+	if (!g_str || ft_strlen(g_str) >= mallocsize)
+	{
+		mallocsize *= 2;
+		tmp = g_str;
+		g_str = ft_calloc(sizeof(char), mallocsize);
+		if (!g_str)
+			return (NULL);
+		if (tmp)
+			ft_strlcpy(g_str, tmp, mallocsize);
+		free(tmp);
+	}
+	g_str[ft_strlen(g_str)] = c;
+	return (g_str);
+}
+
+void	handle_char(char c, int pid, int reset)
+{
+	store_char(c, reset);
+	if (c == 0)
+	{
+		if (ft_printf("%s", g_str) == -1)
+			kill(pid, SIGUSR2);
+		free(g_str);
+		g_str = NULL;
+	}
+	else
+	{
+		if (!store_char(c, reset))
+			kill(pid, SIGUSR2);
+	}
+}
+
+void	sig_handler(int signo, siginfo_t *info, void *context)
 {
 	static int				i = 0;
 	static unsigned char	c = 0;
-	(void)context;
+	static int				pid = 0;
 
+	(void)context;
+	if (pid != info->si_pid)
+	{
+		i = 0;
+		c = 0;
+		free(g_str);
+		g_str = NULL;
+	}
 	if (signo == SIGUSR2)
 	{
-		// ft_printf("received SIGUSR2: 1\n");
 		c <<= 1;
 		c |= 1;
 	}
 	else if (signo == SIGUSR1)
 	{
-		// ft_printf("received SIGUSR1: 0\n");
 		c <<= 1;
 	}
 	i++;
 	if (i == 8)
 	{
-		ft_printf("%c", c);
+		handle_char(c, info->si_pid, pid != info->si_pid);
+		// ft_putchar(c);
 		i = 0;
 		c = 0;
 	}
+	pid = info->si_pid;
 	kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
-	struct sigaction act;
+	struct sigaction	act;
 
 	ft_printf("pid: %d\n", getpid());
 	sigemptyset(&act.sa_mask);
@@ -44,6 +109,6 @@ int	main(void)
 	sigaction(SIGUSR2, &act, NULL);
 	while (1)
 	{
-		pause();
+		sleep(1);
 	}
 }
